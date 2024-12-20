12f39780bf4d426299f766c6459ee117ec337ec3
evm96m@umsystem.edu <em_dev_9836@outlook.com>
2024-12-19 18:10:32 -0600

    moved files from src

--- src/util/configUtil.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-const conf = require("conf");
-// import("conf").then((result) => (conf = result));
-
-module.exports = { conf };
--- src/util/index.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-const { prompt } = require("enquirer");
-const configUtil = require("./configUtil");
-const consoleUtils = require("./consoleUtil");
-const cmd_util = require("./cmdUtil");
-module.id = "util";
-module.exports = { ...consoleUtils, ...configUtil, ...cmd_util, prompt };
